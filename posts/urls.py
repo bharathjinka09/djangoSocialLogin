9391cece82dcfd6cb5ed8366c82b239fb5262749
bharathjinka09 <bharathjinka09@gmail.com>
2020-05-13 15:56:34 +0530

    Added Blog Posts

--- posts/urls.py
+++ posts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home),
     path('accounts/profile/', views.home),
     path('not_logged_in', views.not_logged_in),
+    path('create/', views.create),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -11,6 +12,25 @@ def not_logged_in(request):
 
 @login_required(login_url='/not_logged_in')
 def home(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all()[::-1]
     context = {'posts': posts}
     return render(request, 'home.html', context=context)
+
+
+@login_required(login_url='/not_logged_in')
+def create(request):
+
+    form = PostForm()
+
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = PostForm()
+
+            return redirect('/')
+    else:
+    	form = PostForm()
+
+    context = {'form': form}
+    return render(request, 'create.html', context)
