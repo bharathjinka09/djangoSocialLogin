ba3e92f539e68684f94eb37787b108ce8a61afd9
bharathjinka09 <bharathjinka09@gmail.com>
2020-05-14 17:53:32 +0530

    Added edit post

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,8 @@ from .import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/profile/', views.home),
+    path('accounts/profile/post/<int:pk>/edit/', views.UpdatePostView.as_view()),
+    path('accounts/profile/post/<int:id>/delete/', views.delete_post),
     path('not_logged_in', views.not_logged_in),
     path('create/', views.create),
     path('post/<int:id>/', views.post),
